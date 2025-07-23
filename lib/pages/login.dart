a2165b5d55542db24fda9107a6041ba92abf504b
thmlilojhb024 <thmlilojhb024@student.wethinkcode.co.za>
2025-07-23 11:56:13 +0200

    chore(login page): finished the login UI

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:manga_domain/services/theme.dart';
 import 'package:manga_domain/widgets/custom_checkbox.dart';
 import 'package:manga_domain/widgets/input_box.dart';
+import 'package:manga_domain/widgets/maroon_button.dart';
+import 'package:manga_domain/widgets/red_button.dart';
 import 'package:manga_domain/widgets/underlined_button.dart';
 
 class Login extends StatefulWidget {
@@ -42,6 +44,25 @@ class _LoginState extends State<Login> {
                   InputBox(title: "Password"),
                   CustomCheckbox(),
                   UnderlinedButton(title: "Forgot Password?"),
+                  RedButton(title: "Login", width: 400, height: 45),
+                  SizedBox(height: 10.0),
+                  const Text(
+                    "Or sign in with",
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      color: textSubColor,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 10.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      MaroonButton(title: "Google", width: 150, height: 40),
+                      MaroonButton(title: "Facebook", width: 150, height: 40),
+                    ],
+                  ),
+                  UnderlinedButton(title: "Don't have an account? Sign Up"),
                 ],
               ),
             ),
